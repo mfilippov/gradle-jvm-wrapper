7cac803ef615b4b4cf88bc3affe8a30caa5f15a4
Mikhail Filippov <mikhail@filippov.me>
2019-10-04 12:50:27 +0300

    Add plugin metadata.

---
+++ src/main/kotlin/me/filippov/jvm/wrapper/JvmWrapperPluginExtension.kt
@@ -0,0 +1,10 @@
+package me.filippov.jvm.wrapper
+
+open class JvmWrapperPluginExtension {
+    var winJvmUrl = "https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-windows-x64.zip"
+    var winJvmRelativeArchivePath = "jdk11.0.4_10"
+    var linJvmUrl = "https://repo.labs.intellij.net/cache/https/d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-macosx-x64.tar.gz"
+    var linJvmRelativeArchivePath = ""
+    var macJvmUrl = "https://repo.labs.intellij.net/cache/https/d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-linux-x64.tar.gz"
+    var macJvmRelativeArchivePath = "amazon-corretto-11.jdk/Contents/Home"
+}
