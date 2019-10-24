64fbb1db0f1b7b71d3a18bf8e5c1a6cad430dcc4
Leonid Shalupov <leonid@shalupov.com>
2019-10-24 19:00:01 +0200

    cmd: automatically search directory with java.exe, do not create temp file

--- src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
+++ src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
@@ -2,7 +2,6 @@ package me.filippov.gradle.jvm.wrapper
 
 open class PluginExtension {
     var winJvmUrl = "https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-windows-x64.zip"
-    var winJvmRelativeArchivePath = "jdk11.0.4_10"
     var linJvmUrl = "https://repo.labs.intellij.net/cache/https/d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-linux-x64.tar.gz"
     var linJvmRelativeArchivePath = ""
     var macJvmUrl = "https://repo.labs.intellij.net/cache/https/d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-macosx-x64.tar.gz"
