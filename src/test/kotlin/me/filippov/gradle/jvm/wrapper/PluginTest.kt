ab729d87935cf505f159efa501995d32fbebff5c
Mikhail Filippov <mikhail@filippov.me>
2019-11-04 16:06:16 +0300

    Fix spaces in path.

--- src/test/kotlin/me/filippov/gradle/jvm/wrapper/PluginTest.kt
+++ src/test/kotlin/me/filippov/gradle/jvm/wrapper/PluginTest.kt
@@ -9,10 +9,10 @@ class PluginTest {
     @JvmField
     val testProjectDir = TemporaryFolder()
 
-
     @Test
     fun smoke() {
-        val projectRoot = testProjectDir.root
+        val projectRoot = testProjectDir.newFolder("folder with space")
+        projectRoot.mkdirs()
         withBuildScript(projectRoot) { """
             plugins {
               id("me.filippov.gradle.jvm.wrapper")
@@ -25,11 +25,18 @@ class PluginTest {
         """}
 
         prepareWrapper(projectRoot)
-        val result = gradlew(projectRoot, "hello")
+        val resultWhenJavaNotExists = gradlew(projectRoot, "hello")
 
-        result.stdout.shouldContain("Hello world!")
-        result.stderr.shouldBeEmpty()
-        result.exitCode.shouldBe(0)
+        resultWhenJavaNotExists.stdout.shouldContain("Hello world!", "Invalid output: \n" + resultWhenJavaNotExists.stdout)
+        resultWhenJavaNotExists.stderr.shouldBeEmpty("Invalid output: \n" + resultWhenJavaNotExists.stderr)
+        resultWhenJavaNotExists.exitCode.shouldBe(0)
+
         projectRoot.resolve("build").resolve("gradle-jvm").exists().shouldBeTrue()
+
+        val resultWhenJavaExists = gradlew(projectRoot, "hello")
+
+        resultWhenJavaExists.stdout.shouldContain("Hello world!", "Invalid output: \n" + resultWhenJavaNotExists.stdout)
+        resultWhenJavaExists.stderr.shouldBeEmpty("Invalid output: \n" + resultWhenJavaNotExists.stderr)
+        resultWhenJavaExists.exitCode.shouldBe(0)
     }
 }
