57633a517f06484f62d35fdf064116ae8f80fc12
keykey7 <29210485+keykey7@users.noreply.github.com>
2021-01-28 21:23:34 +0100

    customizable build dir and win url fix

--- src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
+++ src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
@@ -1,6 +1,7 @@
 package me.filippov.gradle.jvm.wrapper
 
 open class PluginExtension {
+	var buildDir = "build"
     var windowsJvmUrl = "https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-windows-x64.zip"
     var linuxJvmUrl = "https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-linux-x64.tar.gz"
     var macJvmUrl = "https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/amazon-corretto-11.0.4.11.1-macosx-x64.tar.gz"
