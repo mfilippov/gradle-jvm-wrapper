c27c7acbbc6df001361e733ca3bc628a92898573
Friedrich von Never <friedrich@fornever.me>
2025-03-03 22:56:38 +0100

    Update to JDK 21

--- src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
+++ src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
@@ -3,9 +3,9 @@ package me.filippov.gradle.jvm.wrapper
 open class PluginExtension {
     var winJvmInstallDir: String = "%LOCALAPPDATA%\\gradle-jvm"
     var unixJvmInstallDir: String = "${"$"}{HOME}/.local/share/gradle-jvm"
-    var windowsX64JvmUrl = "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_windows-x64_bin.zip"
-    var linuxAarch64JvmUrl = "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_linux-aarch64_bin.tar.gz"
-    var linuxX64JvmUrl = "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_linux-x64_bin.tar.gz"
-    var macAarch64JvmUrl = "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_macos-aarch64_bin.tar.gz"
-    var macX64JvmUrl = "https://download.oracle.com/java/17/archive/jdk-17.0.3.1_macos-x64_bin.tar.gz"
+    var windowsX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_windows-x64_bin.zip"
+    var linuxAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_linux-aarch64_bin.tar.gz"
+    var linuxX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_linux-aarch64_bin.tar.gz"
+    var macAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_macos-aarch64_bin.tar.gz"
+    var macX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_macos-x64_bin.tar.gz"
 }
