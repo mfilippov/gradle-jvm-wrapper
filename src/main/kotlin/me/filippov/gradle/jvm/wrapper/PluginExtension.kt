7c86bdd93e5cce9219864f7645ea3c13336e3508
Friedrich von Never <friedrich@fornever.me>
2025-03-08 20:29:47 +0100

    JDK: update to 21.0.5, fix AArch64 version used on x86-64 Linux

--- src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
+++ src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
@@ -3,9 +3,10 @@ package me.filippov.gradle.jvm.wrapper
 open class PluginExtension {
     var winJvmInstallDir: String = "%LOCALAPPDATA%\\gradle-jvm"
     var unixJvmInstallDir: String = "${"$"}{HOME}/.local/share/gradle-jvm"
-    var windowsX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_windows-x64_bin.zip"
-    var linuxAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_linux-aarch64_bin.tar.gz"
-    var linuxX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_linux-aarch64_bin.tar.gz"
-    var macAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_macos-aarch64_bin.tar.gz"
-    var macX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.3_macos-x64_bin.tar.gz"
+    // https://www.oracle.com/java/technologies/javase/jdk21-archive-downloads.html
+    var windowsX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_windows-x64_bin.zip"
+    var linuxAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_linux-aarch64_bin.tar.gz"
+    var linuxX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_linux-x64_bin.tar.gz"
+    var macAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_macos-aarch64_bin.tar.gz"
+    var macX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_macos-x64_bin.tar.gz"
 }
