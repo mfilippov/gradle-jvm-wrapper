41a8022e560c577398f1f4605652cfd3302c14d3
Mikhail Filippov <mikhail@filippov.me>
2026-04-07 21:45:04 +0400

    update default JDK from 21 to 25.0.2

--- src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
+++ src/main/kotlin/me/filippov/gradle/jvm/wrapper/PluginExtension.kt
@@ -3,12 +3,12 @@ package me.filippov.gradle.jvm.wrapper
 open class PluginExtension {
     var winJvmInstallDir: String = "%LOCALAPPDATA%\\gradle-jvm"
     var unixJvmInstallDir: String = "${"$"}{HOME}/.local/share/gradle-jvm"
-    // https://learn.microsoft.com/en-us/java/openjdk/download#openjdk-21
-    var windowsAarch64JvmUrl = "https://aka.ms/download-jdk/microsoft-jdk-21.0.6-windows-aarch64.zip"
-    // https://www.oracle.com/java/technologies/javase/jdk21-archive-downloads.html
-    var windowsX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_windows-x64_bin.zip"
-    var linuxAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_linux-aarch64_bin.tar.gz"
-    var linuxX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_linux-x64_bin.tar.gz"
-    var macAarch64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_macos-aarch64_bin.tar.gz"
-    var macX64JvmUrl = "https://download.oracle.com/java/21/archive/jdk-21.0.5_macos-x64_bin.tar.gz"
+    // https://learn.microsoft.com/en-us/java/openjdk/download#openjdk-25
+    var windowsAarch64JvmUrl = "https://aka.ms/download-jdk/microsoft-jdk-25.0.2-windows-aarch64.zip"
+    // https://www.oracle.com/java/technologies/javase/jdk25-archive-downloads.html
+    var windowsX64JvmUrl = "https://download.oracle.com/java/25/archive/jdk-25.0.2_windows-x64_bin.zip"
+    var linuxAarch64JvmUrl = "https://download.oracle.com/java/25/archive/jdk-25.0.2_linux-aarch64_bin.tar.gz"
+    var linuxX64JvmUrl = "https://download.oracle.com/java/25/archive/jdk-25.0.2_linux-x64_bin.tar.gz"
+    var macAarch64JvmUrl = "https://download.oracle.com/java/25/archive/jdk-25.0.2_macos-aarch64_bin.tar.gz"
+    var macX64JvmUrl = "https://download.oracle.com/java/25/archive/jdk-25.0.2_macos-x64_bin.tar.gz"
 }
